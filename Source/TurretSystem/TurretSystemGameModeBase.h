ffb978fab2e53c01edebcc76d623ac2680a45d70
mehmetemin <kacmazemin72@gmail.com>
2021-03-30 23:13:45 +0300

    Initial commit

---
+++ Source/TurretSystem/TurretSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TurretSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETSYSTEM_API ATurretSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
