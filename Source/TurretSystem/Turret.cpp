7afb4b34b98d8f2fe94099c7a13272d673852184
mehmetemin <kacmazemin72@gmail.com>
2021-04-03 23:21:50 +0300

    feat: add function to find ClosestTarget for turret

--- Source/TurretSystem/Turret.cpp
+++ Source/TurretSystem/Turret.cpp
@@ -2,14 +2,23 @@
 
 
 #include "Turret.h"
+
+#include "DrawDebugHelpers.h"
 #include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 // Sets default values
 ATurret::ATurret()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+
+	TurretSM = CreateDefaultSubobject<UStaticMeshComponent>("TurretStaticMesh");
+	TurretSM->SetupAttachment(RootComponent);
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("BoxCollision");
+	SphereComponent->SetupAttachment(TurretSM);
 }
 
 // Called when the game starts or when spawned
@@ -17,12 +26,51 @@ void ATurret::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TraceObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
+
+	DrawDebugSphere(GetWorld(), GetActorLocation(), SenseRange, 8, FColor::Blue, true, -1.0f, SDPG_World);
+
+}
+
+void ATurret::FindTarget()
+{
+	TArray<AActor*> OverlappingActors;
+
+	const bool IsOverlapped = UKismetSystemLibrary::SphereOverlapActors(GetWorld(), GetActorLocation(),
+																		SenseRange, TraceObjectTypes, nullptr,
+																		ActorsToIgnore, OverlappingActors);
+
+	if(IsOverlapped)
+	{	
+		for (AActor*& HitResult : OverlappingActors)
+		{
+
+			if(!ClosestTarget)
+			{
+				ClosestTarget = HitResult;
+			}
+
+			if(GetDistanceTo(HitResult) < GetDistanceTo(BestTarget))
+			{
+				ClosestTarget = HitResult;
+			}	
+		}
+	}
+	else
+	{
+		ClosestTarget = nullptr;
+	}
+
 }
 
 // Called every frame
 void ATurret::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
+	if(ClosestTarget)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Cyan, ClosestTarget->GetName());
+	}
 }
 
--- Source/TurretSystem/Turret.h
+++ Source/TurretSystem/Turret.h
@@ -3,8 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include "GameFramework/Actor.h"
 #include "Turret.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
 
 UCLASS()
 class TURRETSYSTEM_API ATurret : public AActor
@@ -15,6 +19,24 @@ public:
 	// Sets default values for this actor's properties
 	ATurret();
 
+	UPROPERTY(EditInstanceOnly)
+	UStaticMeshComponent* TurretSM = nullptr;
+
+	UPROPERTY(EditInstanceOnly)
+	USphereComponent* SphereComponent = nullptr;
+
+	UFUNCTION(BlueprintCallable)
+	void FindTarget();
+
+	UPROPERTY(EditInstanceOnly, meta = (ClampMin = "1.0", ClampMax = "1000.0", UIMin = "1.0", UIMax = "1000.0"))
+	float SenseRange = 100.f;
+
+	UPROPERTY(VisibleInstanceOnly)
+	TArray<AActor*> ActorsToIgnore;
+
+	UPROPERTY(VisibleInstanceOnly)
+	TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,4 +45,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	
+	UPROPERTY()
+	AActor* ClosestTarget = nullptr;	
 };
