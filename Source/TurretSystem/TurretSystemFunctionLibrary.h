1f66f9a1409ee5998d304652cbe2681500aa205f
mehmetemin <kacmazemin72@gmail.com>
2021-04-04 23:00:46 +0300

    feat: create FunctionLibrary class

---
+++ Source/TurretSystem/TurretSystemFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/BlueprintFunctionLibraryFactory.h"
+#include "TurretSystemFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETSYSTEM_API UTurretSystemFunctionLibrary : public UBlueprintFunctionLibraryFactory
+{
+	GENERATED_BODY()
+	
+};
