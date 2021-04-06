dec0a7fb9ba158f391335eba4d827dc385cd540d
mehmetemin <kacmazemin72@gmail.com>
2021-04-07 01:41:52 +0300

    feat: change function param

--- Source/TurretSystem/TurretSystemFunctionLibrary.cpp
+++ Source/TurretSystem/TurretSystemFunctionLibrary.cpp
@@ -3,11 +3,15 @@
 
 #include "TurretSystemFunctionLibrary.h"
 
+#include "DrawDebugHelpers.h"
+
 bool UTurretSystemFunctionLibrary::HasLineOfSight(const UObject* WorldContextObject, FHitResult& Hit, const FVector& From,
-    const FVector& To, const TArray<AActor*>& ActorsToIgnore)
+                                                  const FVector& To, const TArray<AActor*>& ActorsToIgnore)
 {
 	FCollisionQueryParams QueryParams;
 	QueryParams.AddIgnoredActors(ActorsToIgnore);
+
+	DrawDebugLine(WorldContextObject->GetWorld(),From,To, FColor::Black,false, 1.f);
 	
 	return !WorldContextObject->GetWorld()->LineTraceSingleByChannel(Hit, From, To, ECC_Visibility, QueryParams);
 }
